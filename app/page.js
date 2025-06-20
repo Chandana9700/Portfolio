968767f73f4a2c8e9d448accdd61014e3a50262c
crajashekhar <crajashekhar@slu.edu>
2025-06-20 14:16:56 -0500

    Fix: converted app/page.js to client component to allow animations

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
+"use client";
+
+import HeroSection from "./components/homepage/hero-section";
+import Blog from "./components/homepage/blog";
 import AboutSection from "./components/homepage/about";
-import Blog from "./components/homepage/blog"; // no props needed now
 import ContactSection from "./components/homepage/contact";
 import Education from "./components/homepage/education";
 import Experience from "./components/homepage/experience";
-import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
 
