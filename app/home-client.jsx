4f70d5d1ff4dfa93c418dfbc58892e54e1518453
crajashekhar <crajashekhar@slu.edu>
2025-06-20 14:22:56 -0500

    Fix: moved homepage into client component wrapper

---
+++ app/home-client.jsx
@@ -0,0 +1,25 @@
+"use client";
+
+import HeroSection from "./components/homepage/hero-section";
+import Blog from "./components/homepage/blog";
+import AboutSection from "./components/homepage/about";
+import ContactSection from "./components/homepage/contact";
+import Education from "./components/homepage/education";
+import Experience from "./components/homepage/experience";
+import Projects from "./components/homepage/projects";
+import Skills from "./components/homepage/skills";
+
+export default function HomeClient() {
+  return (
+    <div suppressHydrationWarning>
+      <HeroSection />
+      <AboutSection />
+      <Experience />
+      <Skills />
+      <Projects />
+      <Education />
+      <Blog />
+      <ContactSection />
+    </div>
+  );
+}
